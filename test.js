47f8f0c7c658eb63af61d5b59a4d33a77f8a6104
cna-cmd <caronatal@outlook.com>
2022-03-07 19:28:13 -0300

    validation

--- test.js
+++ test.js
@@ -4,15 +4,31 @@ const assert = require('assert')
 it('Correctly calculates the sum of 1 and 9', () => {
     assert.equal(operations.sum(1, 9), 10)
 })
+
 it('Correctly calculates the sum of -5 and -1', () => {
     assert.equal(operations.sum(-5, -1), -6)
 })
+
 it('Correctly calculates the difference of 20 and 5', () => {
     assert.equal(operations.subtraction(20, 5), 15)
 })
+
 it('Correctly calculates the product of 30 and 30', () => {
     assert.equal(operations.multiplication(30, 30), 900)
 })
+
 it('Correctly calculates the quotient of 15 and 3', () => {
     assert.equal(operations.split(15, 3), 5)
+})
+
+it('Failure when a string is used instead of a number', () => {
+    assert.equal(operations.validateNumbers('sammy', 5), false)
+})
+
+it('Failure when two strings is used instead of numbers', () => {
+    assert.equal(operations.validateNumbers('sammy', 'sammy'), false)
+})
+
+it('successfully runs when two numbers are used', () => {
+    assert.equal(operations.validateNumbers(2, 2), true)
 })
--- utils.js
+++ utils.js
@@ -2,9 +2,18 @@ const sum = (x, y) => (+x) + (+y);
 const subtraction = (x, y) => +x - +y;
 const multiplication = (x, y) => +x * +y
 const split = (x, y) => +x / +y
+
+const validateNumbers = (x, y) => {
+    if (isNaN(x) || isNaN(y)) {
+        return false
+    }
+    return true
+}
+
 module.exports = {
     sum,
     subtraction,
     multiplication,
-    split
+    split,
+    validateNumbers
 }
