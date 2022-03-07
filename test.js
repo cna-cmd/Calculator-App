daa1756541de8b71865456e2d38731e0bca6c9c6
cna-cmd <caronatal@outlook.com>
2022-03-07 19:20:24 -0300

    split commit

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 const operations = require('./utils.js');
 const assert = require('assert')
 
-it('correctly calculates the sum of 1 and 3', () => {
+it('Correctly calculates the sum of 1 and 9', () => {
     assert.equal(operations.sum(1, 9), 10)
 })
-it('correctly calculates the sum of -1 and -1', () => {
+it('Correctly calculates the sum of -5 and -1', () => {
     assert.equal(operations.sum(-5, -1), -6)
 })
-it('correctly calculates the difference of 33 and 3', () => {
+it('Correctly calculates the difference of 20 and 5', () => {
     assert.equal(operations.subtraction(20, 5), 15)
 })
-it('correctly calculates the product of 12 and 12', () => {
+it('Correctly calculates the product of 30 and 30', () => {
     assert.equal(operations.multiplication(30, 30), 900)
+})
+it('Correctly calculates the quotient of 15 and 3', () => {
+    assert.equal(operations.split(15, 3), 5)
 })
--- utils.js
+++ utils.js
@@ -1,9 +1,10 @@
 const sum = (x, y) => (+x) + (+y);
 const subtraction = (x, y) => +x - +y;
 const multiplication = (x, y) => +x * +y
-
+const split = (x, y) => +x / +y
 module.exports = {
     sum,
     subtraction,
-    multiplication
+    multiplication,
+    split
 }
