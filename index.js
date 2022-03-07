e68f1de62b487b3f44709b6d55512fbba9c3567c
cna-cmd <caronatal@outlook.com>
2022-03-07 19:54:22 -0300

    commit-interface

---
+++ index.js
@@ -0,0 +1,63 @@
+const operations = require('./utils');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+console.log(`
+Welcome to the Node.js Calculator App! 
+
+Usage: The user will be prompted for two numbers, 
+then asked to select their operation of choice.
+`)
+
+rl.question('Enter the first number: ', (x) => {
+    rl.question('Enter the second number: ', (y) => {
+        rl.question(
+            `
+      Please select from the following options:
+      
+      [1] Addition (+)
+      [2] Subtraction (-)
+      [3] Multiplication (*)
+      [4] Division (/)
+      
+      Enter your choice: `,
+            (choice) => {
+                if (!operations.validateNumbers(x, y)) {
+                    console.log('Only numbers are allowed! Please restart the program.')
+                } else {
+                    switch (choice) {
+                        case '1':
+                            console.log(`The sum of ${x} and ${y} is ${operations.sum(x, y)}.`)
+                            break
+                        case '2':
+                            console.log(
+                                `The difference of ${x} and ${y} is ${operations.subtraction(x, y)}.`
+                            )
+                            break
+                        case '3':
+                            console.log(
+                                `The product of ${x} and ${y} is ${operations.multiplication(x, y)}.`
+                            )
+                            break
+                        case '4':
+                            console.log(
+                                `The quotient of ${x} and ${y} is ${operations.split(x, y)}.`
+                            )
+                            break
+                        default:
+                            console.log(
+                                'Please restart the program and select a number between 1 and 4.'
+                            )
+                            break
+                    }
+                }
+                rl.close()
+            }
+        )
+    })
+})
+
